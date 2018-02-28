61653900d06d493b56628f6f1db32e33c87f156d
ccwrc <j.muszelik@gmail.com>
2018-02-28 17:26:22 +0100

    backup

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class HomeController extends Controller
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/")
      */
     public function index()
     {
         return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home()
+    {
+        return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
